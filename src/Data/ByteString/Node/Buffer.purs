e5f9bf92cb52a3b89794e263807e288b4312cb07
Syaiful Bahri <syaiful@pengunjungblog.com>
2017-06-13 07:17:29 +0700

    reexport Node Stream

--- src/Data/ByteString/Node/Buffer.purs
+++ src/Data/ByteString/Node/Buffer.purs
@@ -5,7 +5,10 @@ module Data.ByteString.Node.Buffer
 
 import Prelude
 
-import Data.ArrayBuffer.Types (ArrayBuffer, arrayBuffer, newPtr, length)
+import Control.Monad.Eff (Eff)
+
+import Data.ArrayBuffer.Types (ArrayBuffer, Uint8Array)
+import Data.ArrayBuffer.TypedArray (Ptr(..), arrayBuffer, newPtr, length)
 import Data.ByteString (ByteString(..))
 
 import Node.Buffer (Buffer)
@@ -18,7 +21,7 @@ bufferToUint8Array = unsafeCoerce
 -- | Turn a ByteString to a Buffer, this should only be used when you want to save
 -- | this packet to Node.js Api, eg file system
 toBuffer :: forall r. ByteString -> Eff r Buffer
-toBuffer (ByteString (Ptr a av) n l) = _toBuffer (av + n) l (arrayBuffer av)
+toBuffer (ByteString (Ptr a av) n l) = _toBuffer (a + n) l (arrayBuffer av)
 
 -- | Turn a Buffer to a strict ByteString
 fromBuffer :: Buffer -> ByteString
--- src/Data/ByteString/Node/Stream.purs
+++ src/Data/ByteString/Node/Stream.purs
@@ -1,12 +1,67 @@
-module Data.ByteString.Node.Stream where
+{-
+  This module wrapper around Node.Stream, so we can use ByteString directly to Node.js
+  Stream Api.
+-}
+
+module Data.ByteString.Node.Stream
+  ( onData
+  , onDataEither
+  , read
+  , readEither
+  , write
+  , module Node.Stream
+  ) where
 
 import Prelude
 
-import Node.Stream as S
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+
 import Data.ByteString (ByteString)
 import Data.ByteString.Node.Buffer as NB
 import Data.Either (Either)
+import Data.Maybe (Maybe)
 
-readChunk :: S.Chunk -> Either String ByteString
-readChunk = map NB.fromBuffer <<< S.readChunk
+import Node.Stream
+  ( Stream(), Read(), Readable(), Write(), Writable(), Duplex(), onDataString
+  , setEncoding, onReadable, onEnd, onFinish, onClose, onError, resume, pause
+  , isPaused, pipe, readString, writeString, cork, uncork, setDefaultEncoding, end)
+import Node.Stream as S
 
+onData
+  :: forall w eff
+   . S.Readable w (exception :: EXCEPTION | eff)
+  -> (ByteString -> Eff (exception :: EXCEPTION | eff) Unit)
+  -> Eff (exception :: EXCEPTION | eff) Unit
+onData s f = S.onData s (f <<< NB.fromBuffer)
+
+onDataEither
+  :: forall r eff
+   . S.Readable r (exception :: EXCEPTION | eff)
+  -> (Either String ByteString -> Eff (exception :: EXCEPTION | eff) Unit)
+  -> Eff (exception :: EXCEPTION | eff) Unit
+onDataEither r cb = S.onDataEither r (cb <<< map NB.fromBuffer)
+
+read
+  :: forall w eff
+   . S.Readable w (exception :: EXCEPTION | eff)
+   -> Maybe Int
+   -> Eff (exception :: EXCEPTION | eff) (Maybe ByteString)
+read s = map (map NB.fromBuffer) <<< S.read s
+
+readEither
+  :: forall w eff
+   . S.Readable w eff
+  -> Maybe Int
+  -> Eff eff (Maybe (Either String ByteString))
+readEither s = map (map (map NB.fromBuffer)) <<< S.readEither s
+
+write
+  :: forall r eff
+   . S.Writable r eff
+  -> ByteString
+  -> Eff eff Unit
+  -> Eff eff Boolean
+write s bs finisher = do
+  buf <- NB.toBuffer bs
+  S.write s buf finisher
